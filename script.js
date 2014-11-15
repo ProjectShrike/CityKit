0bae68220cdfb82d7fe6119992bc5b6398f1855c
Sahibjot Saggu <sahibjot@live.ca>
2014-11-15 17:13:52 -0500

    ey

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 	$("#main-search").delay(500).animate({
 		'width' : '560px',
 		'left' : window_width,
-		'padding' : '20px',
-		'padding-top' : '20px'
+		'padding' : '20px'
 	}, 500);
 });
