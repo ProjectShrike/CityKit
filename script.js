78721eab8711ee592ebaceca2b23b04d38fd30fb
Sahibjot Saggu <sahibjot@live.ca>
2014-11-16 09:19:31 -0500

    Changes to the frontend

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 	var sett_butt_show = false;
 	var sports_toggle = false;
 	var concerts_toggle = false;
@@ -48,65 +47,6 @@ $(document).ready(function() {
 
 	$(window).resize(function() {
 		$("#title").css("font-size", $("#title").height());
+		$("#slogan").css("font-size", $("#slogan").height());
 	});
-/*
-	$("#sports").click(function() {
-		//if ($("#"))
-		if (sports_toggle == false) {
-			$("#sports").css("background", "#CCC");
-			sports_toggle = true;
-		}
-		else {
-			$("#sports").css("background", "#FFF");
-			sports_toggle = false;
-		}
-	});
-
-	$("#concerts").click(function() {
-		//if ($("#"))
-		if (concerts_toggle == false) {
-			$("#concerts").css("background", "#CCC");
-			concerts_toggle = true;
-		}
-		else {
-			$("#concerts").css("background", "#FFF");
-			concerts_toggle = false;
-		}
-	});
-
-	$("#stores").click(function() {
-		//if ($("#"))
-		if (stores_toggle == false) {
-			$("#stores").css("background", "#CCC");
-			stores_toggle = true;
-		}
-		else {
-			$("#stores").css("background", "#FFF");
-			stores_toggle = false;
-		}
-	});
-
-	$("#food").click(function() {
-		//if ($("#"))
-		if (food_toggle == false) {
-			$("#food").css("background", "#CCC");
-			food_toggle = true;
-		}
-		else {
-			$("#food").css("background", "#FFF");
-			food_toggle = false;
-		}
-	});
-
-	$("#events").click(function() {
-		//if ($("#"))
-		if (events_toggle == false) {
-			$("#events").css("background", "#CCC");
-			events_toggle = true;
-		}
-		else {
-			$("#events").css("background", "#FFF");
-			events_toggle = false;
-		}
-	});*/
 });
