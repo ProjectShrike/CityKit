b6616b8631dd6fa514d4552cefe4f00ae230c344
Sahibjot Saggu <sahibjot@live.ca>
2014-11-16 03:04:22 -0500

    did a lot of shit

--- script.js
+++ script.js
@@ -10,23 +10,26 @@ $(document).ready(function() {
 
 	$("#butt1").click(function() {
 		$("#title").animate({
-			'top' : '0px'
+			'top' : '5%'
 		}, 1000);
 		$("#butt1").fadeOut("fast");
-		var map_height = String(($(window).height() - 650) / 2) + 'px';
-		$("#map_load").fadeIn();
-		$("#load_bar").animate({
-			'width' : '100%'
-		}, 2000);
-		$("#map_load").delay(2000).animate({
-			'height' : '0px'
-		}).fadeOut();
 		$("#map-canvas").delay(1200).animate({
-			'left' : '10%'
+			'top' : '25%'
 		});
+	});
+
+	$("#settings_butt").click(function(){
+		$("#settings_butt").animate({
+			'right' : '240px'
+		});
+		$("#settings").fadeIn();
 	});
 
 	$(window).resize(function() {
 		$("#title").css("font-size", $("#title").height());
+	});
+
+	$("#sports").click(function() {
+
 	});
 });
