9fdef908e7dd0a910caefc36f3146691a1442df7
Sadman Kazi <sadmansazidk@gmail.com>
2014-11-16 10:35:35 -0500

    Category changes

--- js/curLoc.js
+++ js/curLoc.js
@@ -123,9 +123,6 @@ function setAllMap(map) {
         toggleListener();
     });
 
-function removeMarkers (index) {
-
-}
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see a blank space instead of the map, this
 // is probably because you have denied permission for location sharing.
@@ -135,7 +132,7 @@ function handleEvents (objects) {
     var eventMarker = new Array (objects.length);
     var info = new Array (objects.length);
     for (var i = 0; i < objects.length; i++) {
-        console.log(object[i]);
+        console.log(objects[i]);
         eventGeoListener(eventMarker, info, objects, i);
     }
 }
