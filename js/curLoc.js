ea5b9c2004fd23492bb530ad2857f47890a7666b
Sadman Kazi <sadmansazidk@gmail.com>
2014-11-16 00:44:07 -0500

    made updates to the current locations js file

--- js/curLoc.js
+++ js/curLoc.js
@@ -6,70 +6,14 @@
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see a blank space instead of the map, this
 // is probably because you have denied permission for location sharing.
-/*
+
 var map;
-var radius = 2; //default radius for PoV around the map
+/*var radius = 2; //default radius for PoV around the map
 var zoomVal = Math.log(6371/(radius*1.5))/Math.log(2); //converts the radius into the zoom value
-
-function initialize() {
-    var mapOptions = {
-        zoom: 17
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-
-    // Try HTML5 geolocation
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(location) {
-            var currentPosition = new google.maps.LatLng(location.coords.latitude,
-                location.coords.longitude);
-            var posMarker = new google.maps.Marker ({
-                position: currentPosition,
-                map: map
-            });
-
-            var infowindow = new google.maps.InfoWindow({
-                map: map,
-                position: pos,
-                content: 'You are currently here!'
-            });
-
-            map.setCenter(currentPosition);
-        }, function() {
-            handleNoGeolocation(true);
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleNoGeolocation(false);
-    }
-}
-
-function handleNoGeolocation(errorFlag) {
-    if (errorFlag) {
-        var content = 'Error: The Geolocation service failed.';
-    } else {
-        var content = 'Error: Your browser doesn\'t support geolocation.';
-    }
-
-    var options = {
-        map: map,
-        position: new google.maps.LatLng(60, 105),
-        content: content
-    };
-
-    var infowindow = new google.maps.InfoWindow(options);
-    map.setCenter(options.position);
-}
-
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
 */
-var map;
-
 function initialize() {
     var mapOptions = {
-        zoom: 6
+        zoom: 14
     };
     map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
@@ -79,12 +23,16 @@ function initialize() {
         navigator.geolocation.getCurrentPosition(function(position) {
             var pos = new google.maps.LatLng(position.coords.latitude,
                 position.coords.longitude);
-
+            var marker = new google.maps.Marker({
+                map:map,
+                position: pos
+            })
+            /*
             var infowindow = new google.maps.InfoWindow({
                 map: map,
                 position: pos,
                 content: 'Location found using HTML5.'
-            });
+            });*/
 
             map.setCenter(pos);
         }, function() {
